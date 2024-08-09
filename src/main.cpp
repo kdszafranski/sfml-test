c4ab0795b90dae031f523cff2a6c9e8cddd0ed9d
Kris Szafranski <kris@thoughtshelter.com>
2024-08-09 15:01:20 -0500

    build working with font loading

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,48 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/System/Vector2.hpp>
 
 int main()
 {
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
-    window.setFramerateLimit(144);
+    auto mainWindow = sf::RenderWindow(sf::VideoMode(800, 600, 32u), "CMake SFML asdfProject");
 
-    while (window.isOpen())
+    sf::CircleShape mainShape(200.0f);
+    mainShape.setFillColor(sf::Color::Red);
+    mainWindow.setFramerateLimit(144);
+
+    sf::Font font;
+    if(!font.loadFromFile("assets/fonts/Acme-Regular.ttf")) {
+        return EXIT_FAILURE;
+    }
+
+    sf::Text text;
+    text.setFont(font);
+    text.setString("Woot");
+
+    // set the character size
+    text.setCharacterSize(24); // in pixels, not points!
+
+    // set the color
+    text.setFillColor(sf::Color::White);
+
+    // set the text style
+    text.setStyle(sf::Text::Bold);
+
+    while (mainWindow.isOpen())
     {
-        for (auto event = sf::Event{}; window.pollEvent(event);)
+        for (auto event = sf::Event(); mainWindow.pollEvent(event);)
         {
             if (event.type == sf::Event::Closed)
             {
-                window.close();
+                mainWindow.close();
             }
         }
 
-        window.clear();
-        window.display();
+        // sf::Vector2i mousePos = sf::Mouse::getPosition();
+
+        mainWindow.clear();
+        // mainWindow.draw(mainShape);
+        mainWindow.draw(text);
+        mainWindow.display();
     }
 }
