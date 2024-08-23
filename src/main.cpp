7489798f86c0d289327098e427885dbac4f943ec
Kris Szafranski <kris@thoughtshelter.com>
2024-08-23 14:31:19 -0500

    getting input text on the screen

--- src/main.cpp
+++ src/main.cpp
@@ -2,36 +2,40 @@
 #include <SFML/System.hpp>
 #include <SFML/System/Vector2.hpp>
 #include "util.cpp"
+#include <iostream>
 
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
 
 int main()
 {
-    auto mainWindow = sf::RenderWindow(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, 32u), "CMake SFML asdfProject");
+    auto mainWindow = sf::RenderWindow(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, 32u), "Cool Stuff");
 
-    sf::CircleShape mainShape(200.0f);
+    sf::CircleShape mainShape(16.0f);
     mainShape.setFillColor(sf::Color::Red);
     mainWindow.setFramerateLimit(144);
 
     sf::Font font;
-    if(!font.loadFromFile("assets/fonts/Acme-Regular.ttf")) {
+    if (!font.loadFromFile("assets/fonts/Acme-Regular.ttf"))
+    {
         return EXIT_FAILURE;
     }
 
-    sf::Text text;
-    text.setFont(font);
-    text.setString("Woot");
+    sf::Text mousePosText;
+    mousePosText.setFont(font);
 
     // set the character size
-    text.setCharacterSize(24); // in pixels, not points!
+    mousePosText.setCharacterSize(24); // in pixels, not points!
 
     // set the color
-    text.setFillColor(sf::Color::White);
+    mousePosText.setFillColor(sf::Color::White);
 
     // set the text style
-    text.setStyle(sf::Text::Bold);
+    mousePosText.setStyle(sf::Text::Regular);
 
+    sf::Text userText = mousePosText;
+    userText.setCharacterSize(18);
+    userText.setPosition({ SCREEN_HEIGHT - 20, 0 });
 
     // colors
     float redSteps = SCREEN_WIDTH % 255;
@@ -46,18 +50,35 @@ int main()
             {
                 mainWindow.close();
             }
+
+            if (event.type == sf::Event::TextEntered)
+            {
+                if (event.text.unicode < 128)
+                { 
+                    char n = static_cast<char>(event.text.unicode);
+                    userText.setString( userText.getString() + n );
+                }
+            }
         }
 
         sf::Vector2i mousePos = sf::Mouse::getPosition(mainWindow);
         sf::Vector2f worldMouse = mainWindow.mapPixelToCoords(mousePos);
         // sf::Vector2f worldMouse = mainWindow.mapCoordsToPixel(mousePos);
-        text.setString( std::to_string(redSteps) );
-        text.setString( ts_util::getMousePositionString(worldMouse));
+        // text.setString(std::to_string(redSteps));
+        mousePosText.setString(ts_util::getMousePositionString(worldMouse));
 
-        mainWindow.clear(sf::Color(0, 0, 0));
-        mainWindow.draw(text);
+        mainShape.setPosition(worldMouse);
+
+        mainWindow.clear(sf::Color(0, 0, 20));
+        mainWindow.draw(mousePosText);
+        mainWindow.draw(userText);
+        mainWindow.draw(mainShape);
         mainWindow.display();
 
         color++;
     }
+}
+
+void render()
+{
 }
